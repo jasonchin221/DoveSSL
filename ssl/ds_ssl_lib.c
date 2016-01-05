2b359171a93edba68773905f916142e7b4fedf83
jason <liu_yt@neusoft.com>
2016-01-05 11:22:09 +0800

    save
    Signed-off-by: jason <liu_yt@neusoft.com>

--- ssl/ds_ssl_lib.c
+++ ssl/ds_ssl_lib.c
@@ -1,11 +1,30 @@
 
 #include <openssl/ssl.h>
+#include <openssl/evp.h>
+
 #include "ds_ssl.h"
+#include "ds_errno.h"
+
+extern void ssl_load_ciphers(void);
+extern const EVP_CIPHER *EVP_aes_128_cbc_hmac_sha256(void);
+extern const EVP_CIPHER *EVP_aes_256_cbc_hmac_sha256(void);
 
 int
 ds_library_init(void)
 {
-    return SSL_library_init();
+    EVP_add_cipher(EVP_aes_128_cbc());
+    EVP_add_cipher(EVP_aes_192_cbc());
+    EVP_add_cipher(EVP_aes_256_cbc());
+    EVP_add_cipher(EVP_aes_128_gcm());
+    EVP_add_cipher(EVP_aes_256_gcm());
+    EVP_add_digest(EVP_sha256());
+    EVP_add_digest(EVP_sha384());
+    EVP_add_digest(EVP_sha512());
+    EVP_add_digest(EVP_ecdsa());
+
+    //ssl_load_ciphers();
+
+    return DS_OK;
 }
 
 void
